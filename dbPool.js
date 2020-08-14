21933a76e4b0dba04811ac4573cf1d550ad8572a
EC2 Default User <ec2-user@ip-172-31-13-188.us-east-2.compute.internal>
2020-08-14 09:50:12 +0000

    Changed dbPool credential to mine

--- dbPool.js
+++ dbPool.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "starwars1",
-    database: "final_project"
+    host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "v7pczzk0eqtrq7zl",
+    password: "sa2fs8g9gxfdpvnx",
+    database: "ofnnwyo6iqci6okx"
 });
 
 module.exports = pool;
